99b44314b7d0a7ffea82d036f9c68ab322bf4bcc
iloveket <davidrb2017@outlook.com>
2019-10-15 18:23:41 +0100

    Middleware and protected routes

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function(req, res, next) {
+  // GET TOKEN FROM HEADER
+  const token = req.header('x-auth-token');
+
+  // CHECK IF NOT TOKEN
+  if (!token) {
+    return res.status(401).json({ msg: 'No token; authorization denied' });
+  }
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is invalid' });
+  }
+};
